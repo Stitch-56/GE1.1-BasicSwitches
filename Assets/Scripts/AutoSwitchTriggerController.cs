b4539d7bb10fdb1297e9bc37199343aa08280c2c
Stitch-56 <stitch11497@hotmail.com>
2019-01-17 12:55:50 +0000

    Update

--- Assets/Scripts/AutoSwitchTriggerController.cs
+++ Assets/Scripts/AutoSwitchTriggerController.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class AutoSwitchTriggerController : MonoBehaviour {
 	// Here I have a reference to the ScitchController so that I can turn on and off the switch
 	public SwitchController theSwitch;
+    public BulbController theBulb;
 
 	void OnTriggerEnter2D(Collider2D other) {
 		Debug.Log ("Someone entered the switch trigger");
